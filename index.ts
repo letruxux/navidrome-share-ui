3205965c2ad227f5974b88399b2b0e4f8a5b3028
letruxux <mrgianfranco483@gmail.com>
2026-07-18 18:26:08 +0200

    feat: AH

---
+++ index.ts
@@ -0,0 +1,43 @@
+import Elysia, { status } from "elysia";
+import * as cheerio from "cheerio";
+
+const app = new Elysia();
+
+interface ShareInfo {
+  id: string;
+  description: string;
+  downloadable: boolean;
+  tracks: {
+    id: string;
+    title: string;
+    artist: string;
+    album: string;
+    updatedAt: string;
+    duration: number;
+  }[];
+}
+
+app.get("/extract", async (c) => {
+  const { url } = c.query;
+  if (!url) return status(400, { message: "Missing URL" });
+  /* https://navi.srv.ltrx.lol/share/wpZ4izz7b3 */
+  const html = await fetch(url).then((r) => r.text());
+  const $ = cheerio.load(html);
+  let shareInfo: ShareInfo | null = null;
+  $("script").each((i, el) => {
+    const inner = ($(el).html() || "").trim();
+    if (inner.startsWith("window.__SHARE_INFO__")) {
+      const foundShareInfo = JSON.parse(
+        JSON.parse(inner.replace("window.__SHARE_INFO__ = ", "")),
+      );
+      shareInfo = foundShareInfo;
+    }
+  });
+  if (!shareInfo) return status(404, { message: "Share not found" });
+  return status(200, { shareInfo });
+});
+
+Bun.serve({
+  port: 3006,
+  fetch: app.fetch,
+});
